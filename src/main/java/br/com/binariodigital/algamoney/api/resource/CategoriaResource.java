5d2bafde4bb781fe43da17a955880c3e47017e86
gabrielverasm <gabrielverasm@gmail.com>
2020-04-11 22:53:50 -0300

    Foi implementado inclusão de categoria e consulta por id.

--- src/main/java/br/com/binariodigital/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/br/com/binariodigital/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package br.com.binariodigital.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.binariodigital.algamoney.api.model.Categoria;
 import br.com.binariodigital.algamoney.api.repository.CategoriaRepository;
@@ -20,6 +29,24 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar() {
 		return repository.findAll();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+
+		Categoria categoriaSalva = repository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getcodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+
+	}
+
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		return repository.findById(codigo);
 	}
 
 }
