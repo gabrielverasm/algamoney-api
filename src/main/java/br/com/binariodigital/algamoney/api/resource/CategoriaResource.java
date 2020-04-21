23faf0ec661484e93d2f8b3101675c77d92f9ce3
gabrielverasm <gabrielverasm@gmail.com>
2020-04-21 12:17:15 -0300

    Foi implementado a função de excluir com retorno da api.

--- src/main/java/br/com/binariodigital/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/br/com/binariodigital/algamoney/api/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package br.com.binariodigital.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.binariodigital.algamoney.api.event.RecursoCriadoEvent;
 import br.com.binariodigital.algamoney.api.model.Categoria;
 import br.com.binariodigital.algamoney.api.repository.CategoriaRepository;
 
@@ -26,6 +27,9 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository repository;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Categoria> listar() {
@@ -37,11 +41,9 @@ public class CategoriaResource {
 
 		Categoria categoriaSalva = repository.save(categoria);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(categoriaSalva.getcodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getcodigo()));
 
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 
 	}
 
--- src/main/java/br/com/binariodigital/algamoney/api/resource/PessoaResource.java
+++ src/main/java/br/com/binariodigital/algamoney/api/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package br.com.binariodigital.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,15 +7,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.binariodigital.algamoney.api.event.RecursoCriadoEvent;
 import br.com.binariodigital.algamoney.api.model.Pessoa;
 import br.com.binariodigital.algamoney.api.repository.PessoaRepository;
 
@@ -26,6 +29,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository repository;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Pessoa> listar() {
@@ -37,12 +43,9 @@ public class PessoaResource {
 
 		Pessoa pessoaSalva = repository.save(pessoa);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 
-		response.setHeader("Location", uri.toASCIIString());
-
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 
 	}
 
@@ -54,6 +57,12 @@ public class PessoaResource {
 		return repository.existsById(codigo) ? ResponseEntity.ok().body(pessoaBuscada)
 				: ResponseEntity.notFound().build();
 
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		repository.deleteById(codigo);
 	}
 
 }
